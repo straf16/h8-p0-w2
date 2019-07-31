dc46033ed7cc5f0a81865fd7bc1ceb894b99a936
straf16 <depoksugutamu621@gmail.com>
2019-07-31 10:36:10 +0700

    improvment exercises-3.js

--- exercises-3.js
+++ exercises-3.js
@@ -1,9 +1,9 @@
 var nama = 'Fadil';
 var peran = 'Tabib';
 
-if (nama === '') {
+if ((nama === '') || (nama === undefined)) {
     console.log('Nama harus diisi!');
-} else if (peran === '') {
+} else if ((peran === '') || (peran === undefined)) {
     console.log('Halo ' + nama + ', Pilih peranmu untuk memulai game!');
 } else if (peran === 'Ksatria') {
     console.log('Selamat datang di Dunia Proxytia, ' + nama);
@@ -14,4 +14,6 @@ if (nama === '') {
 } else if (peran === 'Penyihir') {
     console.log('Selamat datang di Dunia Proxytia, ' + nama);
     console.log('Halo Penyihir ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!');
+} else {
+    console.log('Halo ' + nama + ', Pilihan peran hanya: Ksatria, Tabib, Penyihir.');
 }

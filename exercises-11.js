2a8d3eff0a0e1ac75eed68f510e1ca679660e6f3
straf16 <depoksugutamu621@gmail.com>
2019-08-03 12:58:13 +0700

    mengerjakan exercises-11.js

---
+++ exercises-11.js
@@ -0,0 +1,14 @@
+function balikKata(kata) {
+    var balik = '';
+    for (var i = kata.length; i > 0; i--) {
+        balik += kata[i-1];        
+    }
+    return balik;
+  }
+  
+  // TEST CASES
+  console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+  console.log(balikKata('John Doe')); // eoD nhoJ
+  console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+  console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+  console.log(balikKata('Super')); // repuS

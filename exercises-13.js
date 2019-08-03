3b6ff648a25d3b441d98514ec236e1d78f7a3b37
straf16 <depoksugutamu621@gmail.com>
2019-08-03 22:08:42 +0700

    memperbaiki exercises-13

--- exercises-13.js
+++ exercises-13.js
@@ -1,17 +1,22 @@
 function xo(str) {
     var hitungX = 0;
     var hitungO = 0;
+    var something = 0;
     for (let i = 0; i < str.length; i++) {
         if (str[i] === 'x') {
             hitungX++;
         } else if (str[i] === 'o') {
             hitungO++;
+        } else {
+            something++;
         }
     }
-    if (hitungX == hitungO) {
+    if ((hitungX === hitungO) && (something === 0)) {
         return true;
-    } else {
+    } else  if ((hitungX !== hitungO) && (something === 0)) {
         return false;
+    } else if (something !== 0) {
+        return 'terdapat karakter selain x/o'
     }
   }
   
